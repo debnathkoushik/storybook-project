29eccf2c2d24a6db0b4b3e75b51467095abbeb41
Koushik Debnath <debnathkoushik605@gmail.com>
2026-06-24 17:15:25 +0530

    fix(toaster): renders toast component only when length of toasts has changed

--- src/stories/Toaster.js
+++ src/stories/Toaster.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, useMemo } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { Flip } from "gsap/Flip";
@@ -16,77 +16,87 @@ export default function Toaster({ label = "" }) {
   const toasterContainer = useRef(null);
 
   // scope ensures GSAP animations are applied correctly
-  const { contextSafe } = useGSAP(
-    // () => {
-    //   toasterContainer.addEventListener("mouseenter", () => {
-    //     console.log("hovered");
-    //   });
-    //   // const toastsList = gsap.utils.toArray(".toast");
-    //   // console.log("toasts: ", toastsList);
-    //   // toastsList.forEach((toast) => {
-    //   //   toast.addEventListener("mouseenter", () => {
-    //   //     const state = Flip.getState(toast);
-    //   //     gsap.set(toast, {
-    //   //       position: "static",
-    //   //       // clearProps: "top,right,bottom,left,transform",
-    //   //     });
-    //   //     Flip.from(state, {
-    //   //       duration: 0.25,
-    //   //       ease: "power2.out",
-    //   //       absolute: false,
-    //   //     });
-    //   //   });
-    //   //   // toast.addEventListener("mouseleave", () => {
-    //   //   //   const state = Flip.getState(toast);
-    //   //   //   gsap.set(toast, {
-    //   //   //     position: "absolute",
-    //   //   //     right: 16,
-    //   //   //     bottom: 16,
-    //   //   //   });
-    //   //   //   Flip.from(state, {
-    //   //   //     duration: 0.25,
-    //   //   //     ease: "power2.out",
-    //   //   //     absolute: true,
-    //   //   //   });
-    //   //   // });
-    //   // });
-    //   // const toastsList = document.querySelector(".toaster");
-    //   // console.log("toasts: ", toastsList);
-    // },
-    {
-      scope: toasterContainer,
-      // dependencies: [toasts],
-      revertOnUpdate: true,
-    },
-  );
+  // const { contextSafe } = useGSAP(
+  //   // () => {
+  //   //   toasterContainer.addEventListener("mouseenter", () => {
+  //   //     console.log("hovered");
+  //   //   });
+  //   //   // const toastsList = gsap.utils.toArray(".toast");
+  //   //   // console.log("toasts: ", toastsList);
+  //   //   // toastsList.forEach((toast) => {
+  //   //   //   toast.addEventListener("mouseenter", () => {
+  //   //   //     const state = Flip.getState(toast);
+  //   //   //     gsap.set(toast, {
+  //   //   //       position: "static",
+  //   //   //       // clearProps: "top,right,bottom,left,transform",
+  //   //   //     });
+  //   //   //     Flip.from(state, {
+  //   //   //       duration: 0.25,
+  //   //   //       ease: "power2.out",
+  //   //   //       absolute: false,
+  //   //   //     });
+  //   //   //   });
+  //   //   //   // toast.addEventListener("mouseleave", () => {
+  //   //   //   //   const state = Flip.getState(toast);
+  //   //   //   //   gsap.set(toast, {
+  //   //   //   //     position: "absolute",
+  //   //   //   //     right: 16,
+  //   //   //   //     bottom: 16,
+  //   //   //   //   });
+  //   //   //   //   Flip.from(state, {
+  //   //   //   //     duration: 0.25,
+  //   //   //   //     ease: "power2.out",
+  //   //   //   //     absolute: true,
+  //   //   //   //   });
+  //   //   //   // });
+  //   //   // });
+  //   //   // const toastsList = document.querySelector(".toaster");
+  //   //   // console.log("toasts: ", toastsList);
+  //   // },
+  //   {
+  //     scope: toasterContainer,
+  //     // dependencies: [toasts],
+  //     revertOnUpdate: true,
+  //   },
+  // );
 
-  const handleMouseEnter = contextSafe(() => {
-    // const state = Flip.getState(toasterContainer);
-    // console.log("state: ", state);
-    // gsap.set(toast, {
-    //   position: "static",
-    //   // clearProps: "top,right,bottom,left,transform",
-    // });
-    // Flip.from(state, {
-    //   duration: 0.25,
-    //   ease: "power2.out",
-    //   absolute: false,
-    // });
-  });
+  // const handleMouseEnter = contextSafe(() => {
+  //   // const state = Flip.getState(toasterContainer);
+  //   // console.log("state: ", state);
+  //   // gsap.set(toast, {
+  //   //   position: "static",
+  //   //   // clearProps: "top,right,bottom,left,transform",
+  //   // });
+  //   // Flip.from(state, {
+  //   //   duration: 0.25,
+  //   //   ease: "power2.out",
+  //   //   absolute: false,
+  //   // });
+  // });
 
   useEffect(() => {
     setToasts((prev) => [...prev, { label }]);
   }, [label]);
 
+  // ensures Toast does not gets called when the length of toasts has not changed
+  const renderedToasts = useMemo(() => {
+    return toasts.map(({ label }, index) => (
+      <Toast
+        key={index + 1}
+        index={index + 1}
+        label={label}
+        totalToasts={toasts.length}
+      />
+    ));
+  }, [toasts.length]);
+
   return (
     <ol
       className="toaster"
       ref={toasterContainer}
-      onMouseEnter={handleMouseEnter}
+      // onMouseEnter={handleMouseEnter}
     >
-      {toasts.map(({ label }, index) => (
-        <Toast key={index} index={index} label={label} />
-      ))}
+      {toasts.map(({ label }, index) => renderedToasts)}
     </ol>
   );
 }
